ff8363c5771e133e9ca7f6f3516e4d3047f89ecf
John Roque Jorillo <johnroque.jorillo@coreproc.ph>
2019-04-14 03:16:45 +0800

    Initial Commit

---
+++ MediaPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MediaPicker
+//
+//  Created by John Roque Jorillo on 14/04/2019.
+//  Copyright © 2019 John Roque Jorillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
