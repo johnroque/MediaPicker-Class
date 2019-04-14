736edeb3d6f976497a386e9d753220e033c3d0f1
John Roque Jorillo <johnroque.jorillo@coreproc.ph>
2019-04-14 13:12:00 +0800

    Implement MediaPicker

--- MediaPicker/App/Story/Main/ViewController.swift
+++ MediaPicker/App/Story/Main/ViewController.swift
@@ -10,13 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
+    // MARK: Outlets
+    @IBOutlet weak var selectedImageView: UIImageView!
+    @IBOutlet weak var selectImageButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        initViews()
+    }
+    
+    private func initViews() {
+        initButtons()
+    }
+    
+    private func initButtons() {
+        selectImageButton.addTarget(self, action: #selector(showMediaPicker), for: .touchUpInside)
     }
 
+    @objc private func showMediaPicker() {
+        mediaPicker.showMediaSelectionAlert(inViewController: self)
+    }
+    
+    // MARK: Private
+    private lazy var mediaPicker: MediaPickerService = {
+        let config = MediaPickerService.Config.init(placeholder: #imageLiteral(resourceName: "image-placeholder"), supportedSources: [.camera, .gallery])
+        
+        let mediaPicker = MediaPickerService(serviceable: self, config: config)
+        return mediaPicker
+    }()
 
+}
+
+extension ViewController: MediaPickerServiceable {
+    func uploaderAddedFileForUpload(withURL url: URL, withThumbnail thumbnail: UIImage?) {
+        
+        selectedImageView.image = thumbnail
+    }
+    
+    func uploaderAlertCancelled() {
+        
+    }
 }
 
